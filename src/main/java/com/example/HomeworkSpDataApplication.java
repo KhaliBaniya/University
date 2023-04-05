b98bcb8b2ff5e17126a94e5cb75e38f77f320a98
Xumoyun <102345213+KhaliBaniya@users.noreply.github.com>
2023-04-05 13:28:07 +0500

    Project first commit test

---
+++ src/main/java/com/example/HomeworkSpDataApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkSpDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkSpDataApplication.class, args);
+	}
+
+}
