1daaf20fdbf6565db75acdb7daa352c891a715c4
Xumoyun <102345213+KhaliBaniya@users.noreply.github.com>
2023-04-06 09:18:56 +0500

    Dto commited

---
+++ src/main/java/com/example/dto/StudentCourseMarkDTO.java
@@ -0,0 +1,16 @@
+package com.example.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class StudentCourseMarkDTO {
+   private Integer id;
+   private Integer studentId;
+   private Integer courseId;
+   private Integer mark;
+   private LocalDateTime createdDate;
+}
